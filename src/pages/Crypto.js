659720d3c70cfed25b877b7e67797543219a62d0
Marcelo Petry <marcelospetry@gmail.com>
2022-12-29 15:24:20 -0300

    feat: add table component functions

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+// import Filters from "../components/Filters";
+import TableComponent from "../components/TableComponent";
 
 const Crypto = () => {
-  return <div>Crypto</div>;
+  return (
+    <section className="relative mt-16 mb-24 flex h-full w-[80%] flex-col">
+      {/* <Filters /> */}
+      <TableComponent />
+      <Outlet />
+    </section>
+  );
 };
 
 export default Crypto;
