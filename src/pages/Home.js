bb9ad7e7daaf949c4cf14635a39ef6b05fdaf47e
Marcelo Petry <marcelospetry@gmail.com>
2022-12-30 17:05:58 -0300

    fix: fix bug of pages function in index.html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,9 @@ const Home = () => {
     <CryptoProvider>
       <TrendingProvider>
         <StorageProvider>
-          <main className="relative flex flex-col items-center content-center w-full h-full text-white first-letter: font-nunito ">
+          <main
+            className="relative flex flex-col items-center content-center w-full h-full text-white first-letter: font-nunito "
+          >
             <div className="fixed w-screen h-screen bg-gray-300 -z-10" />
             <Logo />
             <Navigation />
