e8c80efed75aea68b31db56fea87138834bb2d88
Marcelo Petry <marcelospetry@gmail.com>
2023-01-02 16:32:04 -0300

    fix: fix navbar overlay on small devices

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,15 +4,15 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav
-      className="mt-20 flex w-[70%] justify-around rounded-lg
-  border border-cyan align-middle
+      className="mt-20 flex justify-around rounded-lg border
+  border-cyan align-middle md:w-[70%]
   "
     >
       <NavLink
         to="/"
         end
         className={({ isActive }) => {
-          return `m-2.5 transform rounded-md px-2 py-1 text-white shadow-lg outline-none transition-transform focus:ring-2 focus:ring-gray-200 active:scale-75 md:px-10 lg:px-20
+          return `m-2.5 ml-4 transform rounded-md px-2 py-1 text-white shadow-lg outline-none transition-transform focus:ring-2 focus:ring-gray-200 active:scale-75 md:px-10 lg:px-20
 ${
   isActive
     ? "bg-cyan text-gray-300"
@@ -42,7 +42,7 @@ ${
       <NavLink
         to="/saved"
         className={({ isActive }) => {
-          return `m-2.5 transform rounded-md px-2 py-1 text-white shadow-lg outline-none transition-transform focus:ring-1 focus:ring-gray-200 active:scale-75 md:px-10 lg:px-20
+          return `m-2.5 mr-4 transform rounded-md px-2 py-1 text-white shadow-lg outline-none transition-transform focus:ring-1 focus:ring-gray-200 active:scale-75 md:px-10 lg:px-20
 ${
   isActive
     ? "bg-cyan text-gray-300"
